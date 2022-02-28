fe7ff2e7b71e1a7a935bc9f6c7e88a15df72c17d
SYdni-31 <zemi1211@gmail.com>
2022-02-28 18:54:06 +0800

    fix little oneproduct gsap & usercenter RWD

--- js/all.js
+++ js/all.js
@@ -93,14 +93,14 @@ $(function(){
        });
 
     // ========商品專頁滑動========
-    let vw =document.body.offsetWidth*1.05;
+    let endvw =document.body.offsetWidth*1.05;
     gsap.registerPlugin(ScrollTrigger);
     gsap.to(".wand1", { 
         rotation: 120,//最後旋轉程度deg
         scrollTrigger: {
         trigger: ".wand1",
-        start: `-300 top`, // (物件開始位置, 卷軸開始位置)
-        end: `+=${vw}`, //(物件結束位置, 卷軸結束位置)
+        start: `-200 top`, // (物件開始位置, 卷軸開始位置)
+        end: `+=${endvw}`, //(物件結束位置, 卷軸結束位置)
         pin: true, // 物件執行完動畫會跟著卷軸走
         scrub: true, // 物件動畫根據卷軸捲動程度跑
       }
